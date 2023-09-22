d7c8620592f268b09449552bf71c57f1ad94eb52
dandredesir <dandredesir@gmail.com>
2023-09-22 13:46:20 -0400

    intern code complete and fixed

--- src/main/java/com/mycompany/agency/Intern.java
+++ src/main/java/com/mycompany/agency/Intern.java
@@ -2,20 +2,24 @@ package com.mycompany.agency;
 
 
 
-    // TODO 1: Make Intern a child of StaffEmploee
+    // TODO 1: Make Intern a child of StaffEmployee
 
-public class Intern extends StaffEmploee
+public class Intern extends StaffEmployee
 {
+
+
+
     //-----------------------------------------------------------------
     //  TODO 2: Constructor: Sets up this intern using the specified
     //  information.
     //-----------------------------------------------------------------
     public Intern(String eName, String eAddress, String ePhone)
     {
-        this.eName = eName;
-        this.eAddress = eAddress;
-        this.ePhone =ePhone;
+
+        super(eName, eAddress, ePhone, "", 0.0);// Pass empty values for unused fields
     }
+
+
 
     //-----------------------------------------------------------------
     // TODO 3:  Returns a zero pay value for this intern.
