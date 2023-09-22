48b80262a9d47d2a7961719628e81f0b2eac441e
dandredesir <dandredesir@gmail.com>
2023-09-22 13:11:57 -0400

    intern code comolete

--- src/main/java/com/mycompany/agency/Intern.java
+++ src/main/java/com/mycompany/agency/Intern.java
@@ -4,7 +4,7 @@ package com.mycompany.agency;
 
     // TODO 1: Make Intern a child of StaffEmploee
 
-public class Intern 
+public class Intern extends StaffEmploee
 {
     //-----------------------------------------------------------------
     //  TODO 2: Constructor: Sets up this intern using the specified
@@ -12,7 +12,9 @@ public class Intern
     //-----------------------------------------------------------------
     public Intern(String eName, String eAddress, String ePhone)
     {
-        
+        this.eName = eName;
+        this.eAddress = eAddress;
+        this.ePhone =ePhone;
     }
 
     //-----------------------------------------------------------------
@@ -20,6 +22,6 @@ public class Intern
     //-----------------------------------------------------------------
     public double pay()
     {
-      
+      return 0.0;
     }
 }
